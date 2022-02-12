bfbfdb720dac0980dab078eef3975fcbcab0cbc1
Johan Halin <johan@halin.me>
2022-02-12 17:51:26 +0200

    Basic mod->wonderswan conversion.
    The header is empty and no samples are written yet.

--- convert_mod.py
+++ convert_mod.py
@@ -14,6 +14,17 @@ ModPattern = namedtuple('ModPattern', ['rows'])
 ModSong = namedtuple('ModSong', ['patterns'])
 Mod = namedtuple('Mod', ['song', 'positions'])
 
+period_map = {
+    # no note
+    0: 0x0,
+    # c-1 to b-1
+    856: 0x1, 808: 0x2, 762: 0x3, 720: 0x4, 678: 0x5, 640: 0x6, 604: 0x7, 570: 0x8, 538: 0x9, 508: 0xa, 480: 0xb, 453: 0xc,
+    # c-2 to b-2
+    428: 0xd, 404: 0xe, 381: 0xf, 360: 0x10, 339: 0x11, 320: 0x12, 302: 0x13, 285: 0x14, 269: 0x15, 254: 0x16, 240: 0x17, 226: 0x18,
+    # c-3 to b-3
+    214: 0x19, 202: 0x1a, 190: 0x1b, 180: 0x1c, 170: 0x1d, 160: 0x1e, 151: 0x1f, 143: 0x20, 135: 0x21, 127: 0x22, 120: 0x23, 113: 0x24
+}
+
 
 def parse_mod_channel_rows(mod_bytes, pattern_index, row_index):
     channel_rows = []
@@ -90,6 +101,45 @@ def parse_mod(input_mod):
     return Mod(song, positions)
 
 
+def write_ws_file(mod, output_file):
+    ws_bytes = bytearray()
+
+    # 32 byte header, actual content TBD
+    for _ in range(0, 32):
+        ws_bytes.append(0)
+
+    # 31 x 32 byte instruments, actual content TBD
+    for _ in range(0, 31):
+        for _ in range(0, 32):
+            ws_bytes.append(0)
+
+    # 256 byte order list, 0xff = end of list
+    for position_index in range(0, 256):
+        max_position = len(mod.positions) - 1
+
+        if position_index <= max_position:
+            ws_bytes.append(mod.positions[position_index])
+        else:
+            ws_bytes.append(0xff)
+
+    # ?? x 1024 byte patterns, 64 rows per pattern, 16 bytes per row
+    for pattern_index, pattern in enumerate(mod.song.patterns):
+        for row_index, row in enumerate(pattern.rows):
+            for channel_index, channel_row in enumerate(row.channel_rows):
+                if channel_row.period in period_map:
+                    ws_bytes.append(period_map[channel_row.period])
+                else:
+                    print('error on pattern {}, row {}, channel {}: unknown period {}'.format(pattern_index, row_index, channel_index, channel_row.period))
+                    ws_bytes.append(0)
+
+                ws_bytes.append(channel_row.sample)
+                ws_bytes.append(channel_row.effect)
+                ws_bytes.append(channel_row.effect_value)
+
+    with open(output_file, mode='wb') as file:
+        file.write(ws_bytes)
+
+
 @click.command()
 @click.argument('input_mod')
 @click.argument('output_file')
@@ -108,6 +158,8 @@ def convert(input_mod, output_file, debug):
 
                 for channel_index, channel in enumerate(row.channel_rows):
                     print('channel {}: {}'.format(channel_index, channel))
+    
+    write_ws_file(mod, output_file)
 
 
 if __name__ == "__main__":
